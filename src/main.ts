dcfa04498e7140d2b2e41fbd95e7d1a36da080d3
gharbiamine <gharbiamine5674@gmail.com>
2022-02-28 17:36:58 +0100

    validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 const corsOptions = {
   origin: ['localhost:3000'],
   optionsSuccessStatus: 200,
@@ -8,6 +9,7 @@ const corsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors(corsOptions);
   await app.listen(3000);
 }
