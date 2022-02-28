f9132fadab4bdf6c6b069b0ffc4cee00b1c10d6c
gharbiamine <gharbiamine5674@gmail.com>
2022-02-28 17:19:57 +0100

    pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as morgan from 'morgan';
+const corsOptions = {
+  origin: ['localhost:3000'],
+  optionsSuccessStatus: 200,
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
